848e1d9140b41a148dfe0a169b600669f76c52ca
caitao <caitaozhan@163.com>
2016-11-13 20:15:02 +0800

    finish js

--- ocr.js
+++ ocr.js
@@ -17,13 +17,16 @@ var ocrDemo = {
     trainArray: [],
     trainingRequestCount: 0,
 
-    onLoadFunction: function(){
+    onLoadFunction: function()
+    {
 	this.resetCanvas();
     },
     
-    resetCanvas: function(){
+    resetCanvas: function()
+    {
 	var canvas = document.getElementById('canvas');
 	var ctx = canvas.getContext('2d');
+	
 	this.data = [];
 	ctx.fillStyle = this.BLACK;
 	ctx.fillRect(0, 0, this.CANVAS_WIDTH, this.CANVAS_WIDTH);
@@ -31,12 +34,139 @@ var ocrDemo = {
 	while(matrixSize--)
 	    this.data.push(0);
 	this.drawGrid(ctx);
+	
+	// event
+	canvas.onmousemove = function(e){this.onMouseMove(e, ctx, canvas)}.bind(this);
+	canvas.onmousedown = function(e){this.onMouseDown(e, ctx, canvas)}.bind(this);
+	canvas.onmouseup = function(e){this.onMouseup(e, ctx, canvas)}.bind(this);
+    },
 
+    drawGrid: function(ctx)
+    {
+	for(var x = this.PIXEL_WIDTH, y = this.PIXEL_WIDTH; x < this.CANVAS_WIDTH; x+=this.PIXEL_WIDTH, y += this.PIXEL_WIDTH)
+	{
+	    ctx.strokeStyle = this.BLUE;
+	    ctx.beginPath();
+	    ctx.moveTo(x, 0);
+	    ctx.lineTo(x, this.CANVAS_WIDTH);
+	    ctx.stroke();
+	    ctx.beginPath();
+	    ctx.moveTo(0, y);
+	    ctx.lineTo(this.CANVAS_WIDTH, y);
+	    ctx.strokeTo();
+	}
+    },
+    
+    onMouseMove: function(e, ctx, canvas)
+    {
+	if(!canvas.isDrawing)
+	{
+	    return;
+	}
+	this.fillSquare(ctx, e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    },
+
+    onMouseDown: function(e, ctx, canvas)
+    {
+        canvas.isDrawing = true;
+        this.fillSquare(ctx, e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+	}
+    },
+
+    onMouseUp: function(e) 
+    {
+	canvas.isDrawing = false;
+    },
+
+    fillsquare: function(ctx, x, y)
+    {
+	var xPixel = Math.floor(x / this.PIXEL_WIDTH);
+	var yPixel = Math.floor(y / this.PIXEL_WIDTH);
+	this.data[((xPixel - 1) * this.TRANSLATED_WIDTH + yPixel) - 1] = 1;
+	
+	ctx.fillStyle = '#ffffff';
+	ctx.fillRect(xPixel * this.PIXEL_WIDTH, yPixel * this.PIXEL_WIDTH, this.PIXEL_WIDTH, this.PIXEL_WIDTH);
+    },
+
+    train: function()
+    {
+	var digitVal = document.getElementById("digit").value;
+	if(!digitVal || this.data.indexOf(1) < 0)
+	{
+	    alert("Pleas type and draw a digit value in order to train the network");
+	    return;
+	}
+
+	// input data to training data set
+	this.trainArray.push({"y0": this.data, "label":parseInt(digitVal)});
+	this.trainingRequestCount++;
+
+	// 
+	if(this.trainingRequestCount == this.BATCH_SIZE)
+	{
+	    alert("Sending training data to server...");
+	    return;
+	
+	    var json = {
+		trianArray: this.trainArray,
+		train: true
+	    };
+
+	    this.sendData(json);
+	    this.trainingRequestCount = 0;
+	    this.trainArray = [];
+	};
+    },
+
+    test: function()
+    {
+	if(this.data.indexOf(1) < 0)
+	{
+	    alert("Pleas draw a digit in order to test the network");
+	    return;
+	}
+
+	var json = {
+	    image: this.data,
+	    predict: true
+	};
+	this.sendData(json);
+    },
+    
+    receiveResponse: function(xmlHttp)
+    {
+	if(xmlHttp.status != 200)
+	{
+	    alert("Server returned status " + xmlHttp.status);
+	    return;
+	}
+	var responseJSON = JSON.parse(xmlHttp.responseText);
+	if(xmlHttp.responseText && responseJSON.type == "test")
+	{
+	    alert("The neural network predicts you wrote a \'" + responseJSON.result + '\'');
+	}
+    },
+
+    onError: function(e)
+    {
+	alert("Error occured while connecting to server: " + e.target.statusText);
+    },
+
+    sendData: function(json)
+    {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open('POST', this.HOST + ":" + this.PORT, false);
+	xmlHttp.onload = function() {this.receiveResponse(xmlHttp)}.bind(this);
+	xmlHttp.onerror = function() {this.onError(xmlHttp)}.bind(this);
+	var msg = JSON.stringify(json);
+	xmlHttp.setRequestHeader('Content-length', msg.length);
+	xmlHttp.setRequestHeader("Connection", "close");
+	xmlHttp.send(msg);
     }
-
-
-
 }
+
+
+
 
 
 
